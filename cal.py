cd532344ba0dc891898697eb0a2a5e989d8a1929
Sanket Sabnavis <122606987+sanketsabnavis@users.noreply.github.com>
2023-04-01 20:17:48 +0530

    Add files via upload
    Basic and simple calendar python code. Python has a built-in module called the Calendar module which allows to perform date, month and calendar-related operations.

---
+++ cal.py
@@ -0,0 +1,7 @@
+import calendar
+year=int(input("Enter the year: "))
+# month=int(input("Enter the month: "))
+# calendar.setfirstweekday(calendar.SUNDAY)
+# mycal=calendar.month(year,month)
+mycal=calendar.calendar(year)
+print(mycal)
